8f2eb7f320ad8d2113bd510125290a6780056504
sergej <sergejgritsenya@gmail.com>
2023-12-11 00:14:00 +0300

    run with args

--- src/common/year-handler.ts
+++ src/common/year-handler.ts
@@ -10,19 +10,30 @@ export class YearHandler {
   }
 
   public execute(): void {
-    const years = fs.readdirSync(this.dirname).filter((item) => !Number.isNaN(Number(item)))
-    for (const year of years) {
-      console.time("executed")
-      this.executeOne(year)
-      console.log("\n")
-      console.timeEnd("executed")
-      console.log("\n")
+    try {
+      const yearArg = process.argv[2]
+
+      const years = yearArg
+        ? [yearArg]
+        : fs.readdirSync(path.resolve(this.dirname)).filter((item) => !Number.isNaN(Number(item)))
+
+      for (const year of years) {
+        console.time("executed")
+        this.executeOne(year)
+        console.log("\n")
+        console.timeEnd("executed")
+        console.log("\n")
+      }
+    } catch (_err) {
+      console.error("Invalid args")
     }
   }
 
   private executeOne(year: string): void {
+    const dayArg = process.argv[3] || ""
+
     console.log(`<<${year}>>\n`)
-    const handlers: DayHandler[] = Object.entries(require(path.resolve(this.dirname, year)))
+    const handlers: DayHandler[] = Object.entries(require(path.resolve(this.dirname, year, dayArg)))
       .sort(([a], [b]) => Number(a.replace("DayHandler", "")) - Number(b.replace("DayHandler", "")))
       .map(([, cls]) => new (cls as any)(year))
 
